530d68082348620c2c5e08c00d1c58d9e26f7a5c
Sudhanshu <kesharwani.sudhanshu1990@gmail.com>
2023-06-13 20:55:03 +0530

    Blank line added

--- pages/contact.py
+++ pages/contact.py
@@ -6,3 +6,4 @@ with st.form(key="my form"):
     st.text_input("Enter your mail", placeholder="Enter e-mail only")
     st.text_area("Enter your query", placeholder="Enter in brief")
     st.form_submit_button("Send")
+
